020384a605e2167cb868086e208151be0194bb1e
eastonfly <eastonfly@gmail.com>
2021-07-11 21:02:55 +0300

    Lesson2 task

--- cmd/hw002/fibon/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/eastonfly/gocourse/cmd/hw002/fibon
-
-go 1.16
--- cmd/hw002/main.go
+++ cmd/hw002/main.go
@@ -8,7 +8,10 @@ import (
 
 func main() {
 	var n int
-	fmt.Println("Vvedi n")
+	fmt.Println("Please, enter the amount:")
 	fmt.Scan(&n)
+	fmt.Println("------------------------------")
+	defer fmt.Println("Here it is the row of",n,"Fibonachi numbers")
+	defer fmt.Println("\n----------------------------")
 	fibon.Printer(n)
 }
